755554553e0ce8f032945dda7c693d98ad8f4815
Apollo13-0 <calderon.ig08@gmail.com>
2021-06-17 23:31:43 -0600

    open directory

--- ceROBOT/mainwindow.cpp
+++ ceROBOT/mainwindow.cpp
@@ -1,5 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "qmessagebox.h"
+#include "QFileDialog"
+#include "QFile"
+#include "QTextStream"
+#include <iostream>
+
+using namespace std;
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -16,5 +23,21 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_actionAbrir_triggered()
 {
+    QFile file;
+    QTextStream io;
+    QString fileName;
+    fileName = QFileDialog::getExistingDirectory(this, "Abrir");
 
+    cout << fileName.toStdString() << endl;
+//    file.setFileName(fileName);
+//    file.open(QIODevice::ReadOnly | QIODevice::Text);
+//    if (!file.isOpen())
+//    {
+//        QMessageBox::critical(this, "Error", file.errorString());
+//        return;
+//    }
+//    io.setDevice(&file);
+//    ui->plainTextEdit->setPlainText(io.readAll());
+//    file.flush();
+//    file.close();
 }
