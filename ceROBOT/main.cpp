fd1c265dcfa835cf57c37c37a9261e159bfb8a6a
Apollo13-0 <calderon.ig08@gmail.com>
2021-06-21 22:57:47 -0600

    add basic logic for server

--- ceROBOT/main.cpp
+++ ceROBOT/main.cpp
@@ -2,6 +2,78 @@
 
 #include <QApplication>
 
+
+#include <iostream>
+#include <string>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <unistd.h>
+#include "json.hpp"
+
+using namespace std;
+
+int StartListenign(){
+
+    int listening = socket(AF_INET, SOCK_STREAM, 0);
+    if (listening == -1)
+    {
+        cerr << "No se pudo crear el Socket. Cerrando..." << endl;
+        return -1;
+    }
+
+    sockaddr_in hint;
+    hint.sin_family = AF_INET;
+    hint.sin_port = htons(11000);
+
+    inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr);
+    bind(listening, (sockaddr*)&hint, sizeof(hint));
+
+    listen(listening, SOMAXCONN);
+
+    while(true){
+        sockaddr_in client;
+        socklen_t clientSize = sizeof(client);
+
+        int clientSocket = accept(listening, (sockaddr *) &client, &clientSize);
+
+        char host[NI_MAXHOST];      // Nombre del cliente remoto
+        char service[NI_MAXSERV];
+
+        memset(host, 0, NI_MAXHOST);
+        memset(service, 0, NI_MAXSERV);
+
+        if (getnameinfo((sockaddr *) &client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0) {
+            cout << host << " conectado " << endl;
+        } else {
+            inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+            cout << host << " conectado al puerto " << ntohs(client.sin_port) << endl;
+        }
+        char buf[4096];
+        memset(buf, 0, 4096);
+
+        int bytesReceived = recv(clientSocket, buf, 4096, 0);
+        if (bytesReceived == -1) {
+            cerr << "Error al recibir mensaje. Cerrando..." << endl;
+            break;
+        }
+
+        if (bytesReceived == 0) {
+            cout << "Cliente desconectado " << endl;
+            break;
+        }
+
+        string messageR = string(buf, 0, bytesReceived);
+        //json jmessageR = json::parse(messageR);
+        //string game = jmessageR.value("Game", "oops");
+
+        close(clientSocket);
+    }
+
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
